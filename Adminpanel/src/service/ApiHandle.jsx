57a052bf792c6720db43ab39e11e0f09abf50788
dilip baghel <dilip.18baghel@gmail.com>
2024-08-14 13:35:41 +0530

    integrate frontend with backend

---
+++ Adminpanel/src/service/ApiHandle.jsx
@@ -0,0 +1,11 @@
+// import api from "../utils/intercepter";
+// import { useEffect, useState } from "react";
+// // api interceptor
+
+// const ApiHandle = () => {
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {}, []);
+// };
+
+// export default ApiHandle;
--- Adminpanel/src/utils/intercepter.js
+++ Adminpanel/src/utils/intercepter.js
@@ -1,25 +1,33 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1/user/delete/3",
+  baseURL: "http://localhost:3000/api/v1/user/",
   timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Add a request interceptor
+// Request interceptor
 api.interceptors.request.use(
   function (config) {
-    // Do something before request is sent
-    console.log(config);
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    console.log("Request sent:", config);
     return config;
   },
+
   function (error) {
     // Do something with request error
+    console.log("Request Error:", error);
     return Promise.reject(error);
   }
 );
 
-// Add a response interceptor
+// Response interceptor
 api.interceptors.response.use(
   function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
@@ -29,6 +37,12 @@ api.interceptors.response.use(
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
+
+    if (error.response.status === 401) {
+      window.location.href = "/login";
+    }
     return Promise.reject(error);
   }
 );
+
+export default api;
